29d2b0bbeab22bcc5118440c2790cbe5aec045e7
Sara Jo <sjo@c7r3s10.42seoul.kr>
2021-12-13 14:16:04 +0900

    final

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,13 +6,15 @@
 /*   By: sjo <sjo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/09 10:50:09 by sjo               #+#    #+#             */
-/*   Updated: 2021/12/03 15:13:53 by sjo              ###   ########.fr       */
+/*   Updated: 2021/12/13 09:23:20 by sjo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *s)
+#include "libft.h"
+
+size_t	ft_strlen(const char *s)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	while (s[i])
--- libft.h
+++ libft.h
@@ -6,15 +6,13 @@
 /*   By: sjo <sjo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 19:51:48 by sjo               #+#    #+#             */
-/*   Updated: 2021/12/06 16:18:03 by sjo              ###   ########.fr       */
+/*   Updated: 2021/12/13 09:13:05 by sjo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-# include <string.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -30,7 +28,7 @@ void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
 void				*ft_memchr(const void *b, int c, size_t n);
 int					ft_memcmp(const void *b1, const void *b2, size_t n);
-int					ft_strlen(const char *s);
+size_t				ft_strlen(const char *s);
 size_t				ft_strlcpy(char *dest, const char *src, size_t dstsize);
 size_t				ft_strlcat(char *dest, const char *src, size_t dstsize);
 char				*ft_strchr(const char *s, int c);
