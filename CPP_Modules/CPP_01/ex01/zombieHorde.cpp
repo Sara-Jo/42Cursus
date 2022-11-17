254391fefad9c1cd15a4cdb3347568d305200dfc
Sara Jo <sr930927@gmail.com>
2022-11-17 17:34:46 +0900

    cpp01 ex01

--- CPP_Modules/CPP_01/ex01/zombieHorde.cpp
+++ CPP_Modules/CPP_01/ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: sjo <sjo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 14:32:05 by sjo               #+#    #+#             */
-/*   Updated: 2022/11/17 14:33:16 by sjo              ###   ########.fr       */
+/*   Updated: 2022/11/17 17:33:28 by sjo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 Zombie* zombieHorde(int N, std::string name) {
     Zombie* horde = new Zombie[N];
     for (int i = 0; i < N; i++)
-        horde[i].setName(name);
+        horde[i].setName(name + std::to_string(i + 1));
     return horde;
 }
