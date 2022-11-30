c8634b597088060a9ec0ecba9697691b836e28ea
Sara Jo <sr930927@gmail.com>
2022-11-30 19:40:47 +0900

    cpp07 ex00

---
+++ CPP_Modules/CPP_07/ex00/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sjo <sjo@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 19:35:38 by sjo               #+#    #+#             */
+/*   Updated: 2022/11/30 19:39:51 by sjo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "whatever.hpp"
+
+int main(void) {
+  int a = 2;
+  int b = 3;
+  ::swap(a, b);
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+  std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+  ::swap(c, d);
+  std::cout << "c = " << c << ", d = " << d << std::endl;
+  std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+  std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+  return 0;
+}
